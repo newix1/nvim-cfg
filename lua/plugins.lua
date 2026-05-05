0121094188238b070156b5223623d5e04787d0b1
newix1 <123808307+newix1@users.noreply.github.com>
2026-05-05 17:10:00 +0500

    Fix Erlang LSP's bug
    Related to https://github.com/saghen/blink.cmp/issues/2081

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -111,6 +111,7 @@ require('blink.cmp').setup({
     },
 
     completion = {
+        accept = { auto_brackets = { enabled = false } },
         documentation = {
             auto_show = true,
             auto_show_delay_ms = 200,
