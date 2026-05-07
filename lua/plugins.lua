3d409eb629e490f05208f9351a38d12bbccb8133
Nikita Fedorov <newix1@users.noreply.github.com>
2026-05-07 20:58:01 +0500

    Add formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -127,3 +127,21 @@ require('blink.cmp').setup({
 
     sources = { default = { "lsp" } }
 })
+
+vim.pack.add({
+  { src = "https://github.com/stevearc/conform.nvim" },
+})
+
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- You can customize some of the format options for the filetype (:help conform.format)
+    rust = { "rustfmt", lsp_format = "fallback" },
+    -- Conform will run the first available formatter
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+    go = { "gofmt" },
+  },
+})
+
