62143b0eda9f7ba5a4e3bb98c9af9fcbb26feef0
newix1 <123808307+newix1@users.noreply.github.com>
2026-05-08 10:53:54 +0500

    Ignore old EDoc format W0038

--- lsp/elp.lua
+++ lsp/elp.lua
@@ -2,4 +2,13 @@ return {
   cmd = { 'elp', 'server' },
   filetypes = { 'erlang' },
   root_markers = { 'rebar.config', 'erlang.mk', '.git' },
+  settings = {
+    elp = {
+      diagnostics = {
+        disabled = {
+          "W0038",
+        }
+      }
+    }
+  },
 }
