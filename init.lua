7e24b02419e3cdf6958fbe4c16f3683739cd41c6
newix1 <123808307+newix1@users.noreply.github.com>
2026-07-06 20:27:04 +0500

    Some refact

--- init.lua
+++ init.lua
@@ -3,6 +3,4 @@ require("plugins")
 require("keymap")
 require("lsp")
 
-vim.cmd.colorscheme "catppuccin-macchiato"
-
 require('lualine').setup()
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,4 @@
 vim.pack.add({
-  { src = "https://github.com/catppuccin/nvim" },
-})
-
-vim.pack.add({
     'https://github.com/nvim-tree/nvim-web-devicons',
     'https://github.com/nvim-lualine/lualine.nvim'
 })
@@ -33,62 +29,6 @@ require("neogit").setup ()
 
 require('gitsigns').setup {
   current_line_blame = true,
-  current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = 'eol',
-    delay = 200, -- мс перед показом
-  },
-  signs = {
-    add = { text = "▎" },
-    change = { text = "▎" },
-    delete = { text = "" },
-    topdelete = { text = "" },
-    changedelete = { text = "▎" },
-    untracked = { text = "▎" },
-  },
-  signs_staged = {
-    add = { text = "▎" },
-    change = { text = "▎" },
-    delete = { text = "" },
-    topdelete = { text = "" },
-    changedelete = { text = "▎" },
-  },
-  on_attach = function(buffer)
-    local gs = package.loaded.gitsigns
-
-    local function map(mode, l, r, desc)
-      vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
-    end
-
-    -- stylua: ignore start
-    map("n", "]h", function()
-      if vim.wo.diff then
-        vim.cmd.normal({ "]c", bang = true })
-      else
-        gs.nav_hunk("next")
-      end
-    end, "Next Hunk")
-    map("n", "[h", function()
-      if vim.wo.diff then
-        vim.cmd.normal({ "[c", bang = true })
-      else
-        gs.nav_hunk("prev")
-      end
-    end, "Prev Hunk")
-    map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
-    map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
-    map({ "n", "x" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-    map({ "n", "x" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-    map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-    map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-    map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-    map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
-    map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-    map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
-    map("n", "<leader>ghd", gs.diffthis, "Diff This")
-    map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-  end,
 }
 
 vim.pack.add({
@@ -101,58 +41,10 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
-vim.pack.add({
-  { src = "https://github.com/m4xshen/hardtime.nvim" },
-})
-
--- require("hardtime").setup()
-
-vim.pack.add({
-    { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1") },
-})
-
-require('blink.cmp').setup({
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
-    signature = { enabled = true },
-    keymap = {
-        preset = "default",
-        ["<C-space>"] = {},
-        ["<C-p>"] = {},
-        ["<Tab>"] = {},
-        ["<S-Tab>"] = {},
-        ["<C-y>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-n>"] = { "select_and_accept" },
-        ["<C-k>"] = { "select_prev", "fallback" },
-        ["<C-j>"] = { "select_next", "fallback" },
-        ["<C-b>"] = { "scroll_documentation_down", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-l>"] = { "snippet_forward", "fallback" },
-        ["<C-h>"] = { "snippet_backward", "fallback" },
-        -- ["<C-e>"] = { "hide" },
-    },
-
-    appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "normal",
-    },
-
-    completion = {
-        accept = { auto_brackets = { enabled = false } },
-        documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 200,
-        }
-    },
-
-    cmdline = {
-        keymap = {
-            preset = 'inherit',
-            ['<CR>'] = { 'accept_and_enter', 'fallback' },
-        },
-    },
-
-    sources = { default = { "lsp" } }
-})
+vim.pack.add({ 'https://github.com/saghen/blink.lib', 'https://github.com/saghen/blink.cmp' })
+local cmp = require("blink.cmp")
+cmp.build():pwait()
+cmp.setup()
 
 vim.pack.add({
   { src = "https://github.com/stevearc/conform.nvim" },
